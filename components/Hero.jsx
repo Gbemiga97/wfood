bae6c34a865f3feb6ddd650b85a43fc3850ba837
Gbemiga97 <holuwagbemiga97@gmail.com>
2023-12-13 19:21:34 +0100

    third commit

---
+++ components/Hero.jsx
@@ -0,0 +1,14 @@
+'use client'
+
+import Image from "next/image"
+import { motion } from "framer-motion"
+
+const Hero = () => {
+  return (
+    <section>
+        Hero
+    </section>
+  )
+}
+
+export default Hero
